251e11d8b3841c4076acba6ac959217ef8e53a42
Matt Wachowski <matt.wachowski@gmail.com>
2020-12-27 10:02:40 -0800

    Added fy driver.
    This works with FY2300, FY6600, FY6800, FY6900 and probably more
    
    The main difference is that it uses read/retries instead of a fixed
    timeout.  This speeds up commands.  In measurement, a 50 point bode
    plot sped up from 112 seconds to 68 seconds.
    
    I named the driver fy.py to keep the fy6600.py file intact and to
    make it clearer that the driver is not limited to the fy6600.  We
    could also set things up differently, if you would prefer.

--- sds1004x_bode/awg_factory.py
+++ sds1004x_bode/awg_factory.py
@@ -10,6 +10,7 @@ from awgdrivers.dummy_awg import DummyAWG
 from awgdrivers.jds6600 import JDS6600
 from awgdrivers.bk4075 import BK4075
 from awgdrivers.fy6600 import FY6600
+from awgdrivers.fy import FygenAWG
 
 class AwgFactory(object):
     
@@ -24,8 +25,13 @@ class AwgFactory(object):
 
 # Initialize factory
 awg_factory = AwgFactory()
-awg_factory.add_awg(DummyAWG.SHORT_NAME, DummyAWG)
-awg_factory.add_awg(JDS6600.SHORT_NAME, JDS6600)
-awg_factory.add_awg(BK4075.SHORT_NAME, BK4075)
-awg_factory.add_awg(FY6600.SHORT_NAME, FY6600)
+drivers = (
+    DummyAWG,
+    JDS6600,
+    BK4075,
+    FY6600,
+    FygenAWG,
+)
+for driver in drivers:
+    awg_factory.add_awg(driver.SHORT_NAME, driver)
 
